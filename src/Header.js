e7e03be98158892f2331f74e08a4c9f130f1665c
nguyendinhduc1102 <nguyendinhduc29041999@gmail.com>
2020-10-07 15:10:44 +0700

    Complated header fontend

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,16 @@
-import { SearchIcon } from '@material-ui/icons';
 import React from 'react';
 import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import FlagIcon from '@material-ui/icons/Flag';
+import StorefrontOutlinedIcon from '@material-ui/icons/StorefrontOutlined';
+import SubscriptionsOutlinedIcon from '@material-ui/icons/SubscriptionsOutlined';
+import SupervisedUserCircleIcon  from '@material-ui/icons/SupervisedUserCircle';
+import { Avatar,IconButton } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import ForumIcon from '@material-ui/icons/Forum';
+import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 function Header() {
     return (
         <div className="header">
@@ -8,15 +18,48 @@ function Header() {
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Facebook-icon-1.png/600px-Facebook-icon-1.png"
                 alt=""/>
                 <div className="header__input">
-                    <input type="text" placeholder="Search facebook"/>
                     <SearchIcon />
+                    <input type="text" placeholder="Search facebook"/>
                 </div>
             </div>
             <div className="header__center">
-
+                <div className="header__option">
+                    <HomeIcon fontSize="large"/>
+                </div>
+                <div className="header__option">
+                    <FlagIcon fontSize="large"/>
+                </div>
+                <div className="header__option">
+                    <SubscriptionsOutlinedIcon fontSize="large"/>
+                </div>
+                <div className="header__option">
+                    <StorefrontOutlinedIcon fontSize="large"/>
+                </div>
+                <div className="header__option">
+                    <SupervisedUserCircleIcon fontSize="large"/>
+                </div>
+                <div className="header__option">
+                    <HomeIcon fontSize="large"/>
+                </div>
             </div>
             <div className="header__right">
-
+                <div className="header__info">
+                    <Avatar />
+                    <h4>NDDDDDDD</h4>
+                </div>
+                <IconButton>
+                    <AddIcon />
+                </IconButton>
+                <IconButton>
+                    <ForumIcon />
+                </IconButton>
+                <IconButton>
+                    <NotificationsActiveIcon />
+                </IconButton>
+                <IconButton>
+                    <ExpandMoreIcon />
+                </IconButton>
+                
             </div>
         </div>
     )
